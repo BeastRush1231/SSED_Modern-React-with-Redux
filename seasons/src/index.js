639ca0265d4d6c124c58f99184c3030441199203
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-03 14:48:53 +0800

    1203 MSP-Section6 componentDidMount

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,20 +4,14 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props){
     super(props);
-    
-    // This is the only time we do direct assignment to this.state
-    this.state = { lat: null, errorMessage: '' };
 
+    this.state = { lat: null, errorMessage: '' };
+  }
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setState
-        this.setState({ lat: position.coords.latitude});
-        // we did not! -----
-        // this.state.lat = position.coords.latitude; 
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      position => this.setState({ lat: position.coords.latitude}),
+      err => this.setState({ errorMessage: err.message })
     );
   }
   
