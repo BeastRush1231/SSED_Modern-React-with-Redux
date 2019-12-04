4ce0732969193c87251b43a6e8cb465377b668cf
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-04 23:29:52 +0800

    1204 MSP-Section6 loading page

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loading from './Loading';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
@@ -21,7 +22,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>  
     }
 
-    return <div>Loading!!</div>
+    return <Loading />
   }
 }
 
