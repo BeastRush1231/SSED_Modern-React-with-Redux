17bc87f76e53a6b5a0b48dc88c2aeee1265eb8e4
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-04 19:45:00 +0800

    1204 MSP-Section6 Ternary Expressions in JSX & Showing Icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -10,8 +10,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div>Latitude: {props.lat}</div> ;
+  const test = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{test}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
