b7680558a24a8a9379829c6021a9b743669788c8
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-06 00:15:09 +0800

    1205 MSP-Section7 pics Ereate Event Handlers

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  onInputChange(event){
+    console.log(event.target.value);
+  }
+
+  onInputClick() {
+    console.log('Input was clicked')
+  }
+
   render() {
     return (
       <div className={`ui segment`}>
         <form className={`ui form`}>
           <div className={`field`}>
             <label htmlFor="search">Image Search</label>
-            <input type="text" name="" id="search"/>
+            <input type="text" onClick={this.onInputClick} onChange={this.onInputChange}/>
           </div>
         </form>  
       </div>
