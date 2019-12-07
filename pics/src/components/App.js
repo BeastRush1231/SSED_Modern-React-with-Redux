de5299f5882c4ceff9ca6cb7968d50fddcf66c75
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-07 16:36:48 +0800

    1207 MSP-Section8 pics Viewing Request results

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos',{
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 3214dc0ca13e3102552436890fc8f1a0d120eb7104981594107c006af1b745bc'
+      }
+    });
   }
 
   render(){
