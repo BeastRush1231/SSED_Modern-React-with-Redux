22deebb6a591e687859b9c6c2291754e21036cb9
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-08 21:40:15 +0800

    1208 MSP-Section9 pics Implementing keys in Lists refactoring review

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -3,9 +3,7 @@ import React from 'react';
 const ImageList = (props) => {
   
   const images = props.images.map( ({description, id, urls}) => {
-    return (
-      <img alt={description} key={id} src={`${urls.regular}`} />
-    )
+    return <img alt={description} key={id} src={`${urls.regular}`} />
   });
 
   return <div>{images}</div>
