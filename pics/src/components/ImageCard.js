345935b194e10cccce99ad585f8480b0cde659f8
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-09 23:47:13 +0800

    1209 MSP-Section9 pics Accessing the DOM with Refs

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
 class ImageCard extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+    // document.querySelector('img')
+    // 取得dom元素
+  }
+
+  componentDidMount() {
+    console.log(this.imageRef);
+  }
+
   render() {
     const { description, urls } = this.props.image;
 
-    return <div>
-      <img 
-        alt={description}
-        src={urls.regular} 
-      /> 
-    </div>
+    return (
+      <div>
+        <img 
+          ref={this.imageRef}
+          alt={description}
+          src={urls.regular} 
+        />
+      </div>
+    )
   } 
 };
 
