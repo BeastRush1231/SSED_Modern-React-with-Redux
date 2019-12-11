98b40dc7cfef077433289b868e54490ba4e027fb
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-11 21:44:36 +0800

    1211 MSP-Section11 videos Searching for Youtube Videos API

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyCCpwGoDSt9ToykPpwktTGfLWplROapFpY';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  //youtube.get(/...);
+  params: {
+    part: 'snippet',
+    maxResults: '5',
+    key: KEY
+  }
+});
