71553d67ad90542e01def89832972f94ef86f4d6
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-11 20:32:57 +0800

    1211 MSP-Section11 videos Reminder on Event Handlers

--- videos/src/component/SearchBar.js
+++ videos/src/component/SearchBar.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+  state = { term: 'test' };
+
+  onInputChange = () => {
+    
+  };
+
   render(){
-    return <div>SearchBar</div>
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="video">Video Search</label>
+            <input 
+              type="text" 
+              id="video" 
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   };
 };
 
