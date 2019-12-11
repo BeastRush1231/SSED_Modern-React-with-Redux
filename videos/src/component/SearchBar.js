1d67e7d5f9a1571361e39b9ddfc80fb5dc102276
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-11 20:39:18 +0800

    1211 MSP-Section11 videos Handling Form Submittal

--- videos/src/component/SearchBar.js
+++ videos/src/component/SearchBar.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
-  state = { term: 'test' };
+  state = { term: '' };
 
-  onInputChange = () => {
-    
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value});
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: Make sure we call
+    // callback from parent component
   };
 
   render(){
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="video">Video Search</label>
             <input 
