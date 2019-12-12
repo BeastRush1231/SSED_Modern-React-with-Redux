0cfa19a0ba2d35ee3ae8c300a4ee56159f655b2f
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-12 21:47:42 +0800

    1212 MSP-Section11 videos Displaying a Video Player

--- videos/src/component/VideoDetail.js
+++ videos/src/component/VideoDetail.js
@@ -4,9 +4,14 @@ const VideoDetail = (props) => {
   if (!props.video){
     return <div>Loading...</div>
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${props.video.id.videoId}` ;
   
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} frameborder="0"></iframe>
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{props.video.snippet.title}</h4>
         <p>{props.video.snippet.description}</p>
