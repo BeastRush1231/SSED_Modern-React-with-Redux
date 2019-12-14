4e19530f26b8e6a044a9fddd4f9277df0ec760a3
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-14 22:08:08 +0800

    1214 MSP-Section25 hook-simple The UseState Hook

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+// import React from 'react';
+import React, {useState} from 'react';
 
-class App extends React.Component {
-  state = { resources: 'init' };
+// class App extends React.Component {
+//   state = { resources: 'init' };
 
-  render() {
-    return(
+// render() {
+const App = () => {
+  const [resource, setResource] = useState('posts'); 
+  
+  return(
+    <div>
       <div>
-        <div>
-          <button onClick={ () => {this.setState({resources: 'posts'})} }>
-            posts
-          </button>
-          <button onClick={ () => {this.setState({resources: 'todos'})} }>
-            todos
-          </button>
-        </div>
-        {this.state.resources}
+        {/* <button onClick={() => {this.setState({resources: 'posts'})} }> */}
+        <button onClick={() => setResource('posts')}>
+          posts
+        </button>
+        {/* <button onClick={() => {this.setState({resources: 'todos'})} }> */}
+        <button onClick={() => setResource('todos')}>
+          todos
+        </button>
       </div>
-    );
-  };
-}
+      {/* {this.state.resources} */}
+      {resource}
+    </div>
+  );
+};
+// }
 
 export default App;
