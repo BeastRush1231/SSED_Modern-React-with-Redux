984a686c2c4887c2f7f1e3dc88405a2635874ceb
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-15 15:32:37 +0800

    1215 MSP-Section25 hook-simple Building the ResourceList review

--- hooks-simple/src/components/ResourcesList.js
+++ hooks-simple/src/components/ResourcesList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class ResourecesList extends React.Component {
   render() {
-  return <div>{this.props.resource}</div>
+    return <div>{this.props.resource}</div>
   };
 }
 
