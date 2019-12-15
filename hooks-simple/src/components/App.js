be66b01fe2ca091238f0997a1f685bb197ce4bb2
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-16 00:11:12 +0800

    1215 MSP-Section25 hook-simple Code Reuse with Hooks axios user

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import ResourcesList from './ResourcesList';
+import UserList from './UserList';
 
 const App = () => {
   const [resource, setResource] = useState('posts'); 
 
   return(
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource('posts')}>posts</button>
         <button onClick={() => setResource('todos')}>todos</button>
