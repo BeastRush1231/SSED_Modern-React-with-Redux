816f61f637f40025cf871b25eb0d7fd1bd50a347
BeastRush1231 <M0761005@g2.usc.edu.tw>
2019-12-16 13:38:15 +0800

    1216 MSP-Section26 Season Refactoring a Class to a Function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,35 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Loading from './Loading';
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude}),
-      err => this.setState({ errorMessage: err.message })
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
     );
+  }, []);
+
+  let content;
+  if (errorMessage) {
+    content = <Loading message="Error Message"/>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Loading message="Please accept location request"/>
   }
 
-  renderContent() {
-    if(this.state.lat && !this.state.errorMessage){
-      return <SeasonDisplay lat={this.state.lat} />
-    }
-
-    if(!this.state.lat && this.state.errorMessage){
-      return <Loading message="Error Message"/>
-    }
-
-    return <Loading message="Please accept location request"/>
-  }
-  
-  render() {
-    return <div className={`border content`}>
-      {this.renderContent()}
-    </div>
-  }
+  return <div className={`border content`}>
+    {content}
+  </div>
 }
 
 ReactDOM.render(
